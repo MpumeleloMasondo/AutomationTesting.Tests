a70e8f0e58b98c8c251647c3d059aaf68098144b
Mpumelelo Masondo <mpumelelo@hooligan.co.za>
2019-01-14 12:55:04 +0200

    Created login test function

---
+++ AutomationTesting.Tests/LoginTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace AutomationTesting.Tests
+{
+    [TestFixture]
+    public class LoginTests : TestBase
+    {
+
+        [Test]
+        public void VerifyValidLogin()
+        {
+            driver.Url = "http://client.racsystems.co.za/Account/Login";
+
+            //Enter credentials
+            driver.FindElement(By.Id("LoginName")).SendKeys("sp12ikeyhair4me"); //Password
+            driver.FindElement(By.Id("Password")).SendKeys("ricroc123");
+
+            driver.FindElement(By.ClassName("btn-login")).Click();
+
+            //enter validations
+            //Assert.That()
+        }
+        [Test]
+        public void VerifyInValidLogin()
+        {
+
+        }
+    }
+}
---
+++ AutomationTesting.Tests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AutomationTesting.Tests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            driver = new FirefoxDriver("E:/inetpub/wwwroot/TestAutomation/AutomationTesting.Tests/AutomationTesting.Tests/bin/Debug"); // Launches browser
+            driver.Manage().Window.Maximize(); // Maximise browser window
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // Set locatorTimeout to be 20n secs at max
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
